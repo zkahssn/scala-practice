40b2b8a5a022e31ad32f4d19b578dcf1db68670e
Zakia Hossain <zakiahossain01@gmail.com>
2019-06-27 00:44:12 +0100

    findPenultimate

--- ListFunctions.scala
+++ ListFunctions.scala
@@ -3,9 +3,18 @@ object ListFunctions{
 
 def findLast(list : List[Int]) : Int ={
 	list match {
-	case h::Nil => h
-	case _::tail => findLast(tail)
+		case h::Nil 	=> h
+		case _::tail 	=> findLast(tail)
 	}
+}
+
+def findPenultimate(list : List[Int]):Int ={
+	list match {
+		case h::i::Nil 	=> h
+		case _::tail 	=> findPenultimate(tail)
+		case _ 			=> 0
+	}
+
 }
 
 }
